84a2b02c2c5bfd9453c860f70c143f6cde394b2c
Ben Woodfield <raser.13@hotmail.com>
2016-12-02 00:01:54 +0000

    pretty much rewrote the whole thing
    I am not sure what happened to the indentation. I worked on this in my offline editor so I could actually test it out, and all I did was come back to the repo, edit the py file and delete the old version, I then copied and pasted the new code into it, but I got a wierd indentation (it stayed at 4 spaces but was showing underlines on everything like it was wrong, I had a look and this editor wants 8 spaces it seems.)

--- magic_number_trick.py
+++ magic_number_trick.py
@@ -3,109 +3,136 @@
 # Written & Coded by Ben Woodfield 02/01/2016
 #(First Program Written with no help or guide)
 
-# INSTRUCTIONS OF THE ORIGINAL GAME
-#1: The player is given 6 cards (the original trick had actual cards), each has a grid with 32 numbers.
-#2: The player is asked to pick a number from any of the cards.
-#3: Only the cards with the chosen number on are to be kept - the others aren't needed
-#4: The remaining cards' top left numbers (top left corners) will equal the number chosen
-#%: It works for ANY number that is chosen!
+''' INSTRUCTIONS OF THE ORIGINAL GAME
+          1: The player is given 6 cards (the original trick had actual cards), each has a grid with 32 numbers.
+          2: The player is asked to pick a number from any of the cards.
+          3: Only the cards with the chosen number on are to be kept - the others aren't needed
+          4: The remaining cards' top left numbers (top left corners) will equal the number chosen
+          5: It works for ANY number that is chosen! '''
+
+# 01/12/2016 I opened this up to improve it. I put a question on Stack Overflow asking 
+# for a way to display the grid in a neater fashion here it is:
+# (http://stackoverflow.com/questions/40915353/how-to-display-a-grid-of-numbers-neatly/40915777#40915777)
+# The simplest solution was given to me in the form of a function to show the grid 
+# in a 4 x 6 structure. It worked so I applied it to all of the grids. Then pretty much 
+# rewrote the whole thing.
+
 
 #!/usr/bin/env python
 import time
 
-GRID_1 = ['32....33....34....35....36....37....38....39....',
-          '40....41....42....43....44....45....46....47....',
-          '48....49....50....51....52....53....54....55....',
-          '56....57....58....59....60....61....62....63....']
+GRID_1 = ['32....33....34....35 \n36....37....38....39',
+          '\n40....41....42....43 \n44....45....46....47',
+          '\n48....49....50....51 \n52....53....54....55',
+          '\n56....57....58....59 \n60....61....62....63\n\n']
+
+GRID_2 = ['8.....9.....10....11 \n12....13....14....15',
+          '\n24....25....26....27 \n28....29....30....31',
+          '\n40....41....42....43 \n44....45....46....47',
+          '\n56....57....58....59 \n60....61....62....63\n\n']
 
 
-GRID_2 = ['8.....9.....10....11....12....13....14....15....',
-          '24....25....26....27....28....29....30....31....',
-          '40....41....42....43....44....45....46....47....',
-          '56....57....58....59....60....61....62....63....']
+GRID_3 = ['4.....5.....6.....7  \n12....13....14....15',
+          '\n20....21....22....23 \n28....29....30....31',
+          '\n36....37....38....39 \n44....45....46....47',
+          '\n52....53....54....55 \n60....61....62....63\n\n']
 
 
-GRID_3 = ['4.....5.....6.....7.....12....13....14....15....',
-          '20....21....22....23....28....29....30....31....',
-          '36....37....38....39....44....45....46....47....',
-          '52....53....54....55....60....61....62....63....']
+GRID_4 = ['2.....3.....6.....7  \n10....11....14....15',
+          '\n18....19....22....23 \n26....27....30....31',
+          '\n34....35....38....39 \n42....43....46....47',
+          '\n50....51....54....55 \n58....59....62....63\n\n']
 
 
-GRID_4 = ['2.....3.....6.....7.....10....11....14....15....',
-          '18....19....22....23....26....27....30....31....',
-          '34....35....38....39....42....43....46....47....',
-          '50....51....54....55....58....59....62....63....']
+GRID_5 = ['16....17....18....19 \n20....21....22....23',
+          '\n24....25....26....27 \n28....29....30....31',
+          '\n48....49....50....51 \n52....53....54....55',
+          '\n56....57....58....59 \n60....61....62....63\n\n']
 
 
-GRID_5 = ['16....17....18....19....20....21....22....23....',
-          '24....25....26....27....28....29....30....31....',
-          '48....49....50....51....52....53....54....55....',
-          '56....57....58....59....60....61....62....63....']
+GRID_6 = ['1.....2.....5.....7\n9.....11....13...15',
+          '\n17....19....21...23  \n25....27....29...31',
+          '\n33....35....37...39  \n41....43....45...47',
+          '\n49....51....53...55  \n57....59....61...63\n\n']
 
 
-GRID_6 = ['1.....2.....5.....7.....9.....11....13....15....',
-          '17....19....21....23....25....27....29....31....',
-          '33....35....37....39....41....43....45....47....',
-          '49....51....53....55....57....59....61....63....']
+
+# The function prints a grid - this came from a response to
+# a question I asked on stack
+def grid_print(GRID_1):
+    for x in zip(*[iter(GRID_1)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
+
+def grid_print(GRID_2):
+    for x in zip(*[iter(GRID_2)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
+
+def grid_print(GRID_3):
+    for x in zip(*[iter(GRID_3)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
 
 
-userName = raw_input("Welcome to the Magic Grid - Number Trick! Please enter your name >>>  ")
-time.sleep(2)
-print "Welcome to the program,", userName
-goAgain = 1
-print ("Take a quick look at the 6 number-grids below...")# I don't know how to insert a table, grid or chart to display the numbers - for now they appear in a bit of a messy fashion
-time.sleep(2)
-goAgain = 1
-print "\n\nHere is Grid Number - 1\n\n"
-print (GRID_1)
-time.sleep(1)
-print "\n\nHere is Grid Number - 2\n\n"
-print (GRID_2)
-time.sleep(1)
-print "\n\nHere is Grid Number - 3\n\n"
-print (GRID_3)
-time.sleep(1)
-print "\n\nHere is Grid Number - 4\n\n"
-print (GRID_4)
-time.sleep(1)
-print "\n\nHere is Grid Number - 5\n\n"
-print (GRID_5)
-time.sleep(1)
-print "\n\nHere is Grid Number - 6\n\n"
-print (GRID_6)
-time.sleep(2)
+def grid_print(GRID_4):
+    for x in zip(*[iter(GRID_4)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
 
-print "\n\nPick any Number from One of the Grids...and remember the grid it was from...\n\n"
-time.sleep(2)
-
-print "Please enter your chosen Number:"
-user_reply = raw_input('>>>')
-time.sleep(2)
-print "Thank You...now for this trick to work you need to concentrate ONLY on the grids which have YOUR chosen NUMBER in..."
-time.sleep(3)
-print "Please enter the Grid Numbers containing your number - (including the one you chose) - (for example enter 1, 2, 3)"
-user_reply = raw_input('>>>')
-
-print "OK, Well Done...Now, I want you to look at the grids you have chosen, and take note of their first numbers - you will need to enter these below"
-time.sleep(6)
-
-firstNumber = int(raw_input("Pease enter your First Number >"))
-secondNumber = int(raw_input("Please enter your Second Number >"))
-thirdNumber = int(raw_input("Please enter your Third Number - type 0 if you don't have one >"))
-fourthNumber = int(raw_input("Please enter your Fourth Number - type 0 if you don't have one >"))
-print ("\nThe sum of these numbers should result in the Number You Chose at the start\n")
-time.sleep(4)
-print "\nYour Number Is.......\n"
-time.sleep(2)
-print (firstNumber + secondNumber + thirdNumber + fourthNumber)
-time.sleep(2)
+def grid_print(GRID_5):
+    for x in zip(*[iter(GRID_5)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
 
 
-#input("\n\nPress the enter key to exit.")#THIS BASIC EXIT FUNCTION WORKS (however the program should have a loop style function to ask another question or quit - when I know how to do this properly I will update it)
-# The lines of code below were taken from a magic8ball program
-# the code is supposed to ask the user if they want to play again 
-# if [yes] is typed, it loops to the start again, and anything else typed quits the program
-# I changed the printed writing to fit with this program, but it didnt even work on the 8ball program
+def grid_print(GRID_6):
+    for x in zip(*[iter(GRID_6)]*4):
+        print('{:<4}{:^6}{:^6}{:^6}'.format(*x))
+
+def questions():
+        print('|---Welcome to the Magic Number Trick---| \nFollow the simple instructions to play.')
+        time.sleep(0.80)
+        name = input('What is your name? \n>>> ')
+        time.sleep(0.5)
+        print("Hi %s let's get started..." % name)
+        time.sleep(1.5)
+        print('I am going to show you 6 grids of numbers...\n\nThen ask you to pick a number between 1 and 63 from one of the grids.')
+        cont1 = input("Press Enter to continue:")
+        print("Here is Grid 1: \n")
+        grid_print(GRID_1)
+        time.sleep(0.75)
+        print("Here is Grid 2: \n")
+        grid_print(GRID_2)
+        time.sleep(0.75)
+        print("Here is Grid 3: \n")
+        grid_print(GRID_3)
+        time.sleep(0.75)
+        print("Here is Grid 4: \n")
+        grid_print(GRID_4)
+        time.sleep(0.75)
+        print("Here is Grid 5: \n")
+        grid_print(GRID_5)
+        time.sleep(0.75)
+        print("Here is Grid 6: \n")
+        grid_print(GRID_6)
+        time.sleep(0.75)
+        cont =  input("\n\nPick a number...Keep it to yourself... \n\nThen press Enter to continue when ready...\n\n|-----------------------------------------|\n\n")
+        time.sleep(2)
+        print("Thank You...now for this trick to work you need \n\nto concentrate ONLY on the grids which have \n\nYOUR chosen NUMBER in...")
+        time.sleep(1.5)
+        print("Have a look through the grids...\n\nSee which ones have your number, \n\nIncluding the grid you got your number from")
+        time.sleep(1.5)
+        cont2 = input("Then press Enter to continue")
+        print("\n\n\nYou are going to enter the FIRST number from each grid \n\ncontaining your number")
+        time.sleep(1)
+        print ("Follow the next set of instructions carefully... \nEnter the numbers one at a time:")
+        time.sleep(2)
+        firstNumber = int(input("Pease enter your First Number \n>>> "))
+        secondNumber = int(input("Please enter your Second Number \n>>> "))
+        thirdNumber = int(input("Please enter your Third Number - type 0 if you don't have one \n>>> "))
+        fourthNumber = int(input("Please enter your Fourth Number - type 0 if you don't have one \n>>>"))
+        time.sleep(4)
+        print ("\nYour Number Is.......\n")
+        time.sleep(2)
+        print (firstNumber + secondNumber + thirdNumber + fourthNumber)
+        time.sleep(2)
+
 
 def final():
         print("Thanks For Playing Magic Number!...Would you like to play again?...Type [yes] to continue, or anything else to quit")
@@ -117,6 +144,8 @@ def final():
                         print("Thanks for playing Magic Number!")
                         sys.exit(0)
 
+
+questions()
 final() 
 
 
